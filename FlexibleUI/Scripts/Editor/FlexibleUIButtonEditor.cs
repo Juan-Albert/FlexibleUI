da8673538fc2491931068d276daabb2b52e08577
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2019-12-17 20:12:05 +0100

    Flexible UI

---
+++ FlexibleUI/Scripts/Editor/FlexibleUIButtonEditor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+using System;
+using CamperoEditorTool;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(FlexibleUIButton))]
+public class FlexibleUIButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        var myScript = target as FlexibleUIButton;
+        
+        GUI.enabled = false;
+        EditorGUILayout.ObjectField("Script", myScript, typeof(FlexibleUIButton), false);
+        GUI.enabled = true;
+        
+        EditorGUILayout.LabelField("Button Config", EditorStyles.boldLabel);
+
+        myScript.buttonTypes = (FlexibleUIData.BUTTON_TYPES) EditorGUILayout.EnumPopup("Button Type", myScript.buttonTypes);
+/*
+        bool paint = false;
+        using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(myScript.buttonTypes == FlexibleUIData.BUTTON_TYPES.None)))
+        {
+            if (group.visible == false)
+            {
+                paint = true;
+            }
+        }
+    */
+        if (myScript.buttonTypes != FlexibleUIData.BUTTON_TYPES.None)
+        {
+            EditorGUI.indentLevel++;
+            myScript.buttonMode = (ButtonMode) EditorGUILayout.EnumPopup("Button Mode", myScript.buttonMode);
+            myScript.buttonTransition = (Selectable.Transition) EditorGUILayout.EnumPopup("Button Transition", myScript.buttonTransition);
+            myScript.imageByColor = EditorGUILayout.Toggle("Image By Color", myScript.imageByColor);
+            EditorGUI.indentLevel--;
+        }
+        
+        EditorGUILayout.LabelField("Button Tween", EditorStyles.boldLabel);
+
+        myScript.hasPressTween = EditorGUILayout.Toggle("Has Press Tween", myScript.hasPressTween);
+
+        if (myScript.hasPressTween)
+        {
+            EditorGUI.indentLevel++;
+            myScript.pressed = EditorGUILayout.Vector3Field("Pressed Scale", myScript.pressed);
+            myScript.duration = EditorGUILayout.FloatField("Tween Duration", myScript.duration);
+            EditorGUI.indentLevel--;
+            EditorGUILayout.Space();
+        }
+
+        EditorGUILayout.LabelField("Button Sound", EditorStyles.boldLabel);
+
+
+        myScript.hasSound = EditorGUILayout.Toggle("Has Sound", myScript.hasSound);
+
+        if (myScript.hasSound)
+        {
+            EditorGUI.indentLevel++;
+            myScript.soundTapType = (SoundTapType) EditorGUILayout.EnumPopup("Sound Type", myScript.soundTapType);
+            EditorGUI.indentLevel--;
+            
+        }
+        
+        if(GUI.changed)
+            myScript.OnValidate();
+    }
+}
---
+++ FlexibleUI/Scripts/Editor/FlexibleUIInstance.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FlexibleUIInstance : Editor
+{
+    [MenuItem("GameObject/Flexible UI/Button", priority = 0)]
+    public static void AddButton()
+    {
+        Create("Button", "FlexibleUIPrefabs");
+    }
+    
+    [MenuItem("GameObject/Flexible UI/Text Label", priority = 1)]
+    public static void AddText()
+    {
+        Create("Text", "FlexibleUIPrefabs");
+    }
+    
+    [MenuItem("GameObject/Flexible UI/Panel", priority = 2)]
+    public static void AddPanel()
+    {
+        Create("Panel", "FlexibleUIPrefabs");
+    }
+    
+    [MenuItem("GameObject/Flexible UI/Scroll View/Vertical", priority = 3)]
+    public static void AddTopScrollView()
+    {
+        Create("VerticalScrollView", "FlexibleUIPrefabs");
+    }
+    
+    [MenuItem("GameObject/Flexible UI/Scroll View/Horizontal", priority = 4)]
+    public static void AddRightScrollView()
+    {
+        Create("HorizontalScrollView", "FlexibleUIPrefabs");
+    }
+    
+    [MenuItem("GameObject/Flexible UI/Scroll View/Grid", priority = 5)]
+    public static void AddGridScrollView()
+    {
+        Create("GridScrollView", "FlexibleUIPrefabs");
+    }
+    
+    private static GameObject clickedObject;
+
+    private static GameObject Create(string objectName, string folder)
+    {
+        GameObject instance = Instantiate(Resources.Load<GameObject>(folder + "/" + objectName));
+        instance.name = objectName;
+        clickedObject = UnityEditor.Selection.activeObject as GameObject;
+
+        if (clickedObject != null)
+        {
+            instance.transform.SetParent(clickedObject.transform, false);
+        }
+        //PrefabUtility.UnpackPrefabInstance(instance, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+
+        return instance;
+    }
+}
